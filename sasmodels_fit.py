8e473f5b65d7dbd7c3e8021b4c66843e1175819e
Wojciech Potrzebowski <Wojciech.Potrzebowski@esss.se>
2022-11-30 14:45:02 +0100

    Changing execution script

--- sasmodels_fit.py
+++ sasmodels_fit.py
@@ -1,33 +1,32 @@
 from sasmodels.core import load_model
 from sasmodels.bumps_model import Model, Experiment
-#from sasmodels.data import load_data
 from sasdata.dataloader.loader import Loader
 
 from bumps.names import *
 from bumps.fitters import fit
-from bumps.formatnum import format_uncertainty
+#from bumps.formatnum import format_uncertainty
 
 
 loader = Loader()
-data = loader.load('test.h5')
+data = loader.load('test.nxs')
 #data = load_data('test_data.txt')
 
 data = data[0]
-data.qmin = min(data.x)
-data.qmax = max(data.x)
+data.qmin = 0.0229
+data.qmax = 0.151
 
-kernel = load_model('cylinder')
-pars = dict(radius=35,
-            length=350,
-            background=0.0,
-            scale=1.0,
-            sld=4.0,
-            sld_solvent=1.0)
+kernel = load_model('sphere@hardsphere')
+pars = dict(radius=29,
+            background=18.0,
+            scale=2.96,
+            sld=3.83,
+            sld_solvent=14.73,
+            radius_effective=36.72,
+            volfraction=0.258)
 model = Model(kernel, **pars)
 
 # SET THE FITTING PARAMETERS
-model.radius.range(1, 50)
-model.length.range(1, 500)
+model.radius.range(25, 35)
 
 M = Experiment(data=data, model=model)
 problem = FitProblem(M)
